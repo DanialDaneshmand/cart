d9b8570640efcca0628a388abae2ab3e0c40ff91
danial daneshmand <daniald11111377@gmail.com>
2023-07-13 05:13:24 -0700

    styles of navbar added

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { useProducts } from "../Context/CounterProvider";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  const products = useProducts();
+  return (
+    <div className={styles.navbar}>
+      <span style={{ fontSize: "25px" }}>cart</span>
+      <p>{`(${products.length})`}</p>
+    </div>
+  );
+};
+
+export default Navbar;
